c71c932143f4c12927411a2a1f0531ff9c482326
juds-lee <judy_96_@hotmail.com>
2022-07-21 12:11:35 -0400

    some changes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,24 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
 
 const Dashboard = () => {
+    const [error, setError] = useState("");
+    function handleLogOut(){
+
+    }
+
     return(
     <>
-     hello world
+    <Card>
+        <Card.Body> 
+            <h2>TITLE HERE</h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+
+        </Card.Body>
+    </Card>
+     <div className="w-100 text-center mt-2">
+       <Button variant="link" onclick={handleLogOut}>Log Out</Button> 
+      </div>
     </>
     )
 }
